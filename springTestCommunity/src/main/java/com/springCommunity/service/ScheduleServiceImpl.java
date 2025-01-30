a00f8fbcc50471fae5739eb109d911f94917d521
ParkInJae <pak293100@jbnu.ac.kr>
2025-01-31 05:59:50 +0900

    Update ScheduleServiceImpl.java
    Update ScheduleServiceImpl.java

--- springTestCommunity/src/main/java/com/springCommunity/service/ScheduleServiceImpl.java
+++ springTestCommunity/src/main/java/com/springCommunity/service/ScheduleServiceImpl.java
@@ -5,36 +5,33 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.springCommunity.dao.ScheduleServiceDAO;
+import com.springCommunity.dao.ScheduleDAO;
 import com.springCommunity.vo.ScheduleVO;
 
 @Service
 public class ScheduleServiceImpl implements ScheduleService{
 	
 	@Autowired
-	private ScheduleServiceDAO scheduleServiceDAO ;
-
-	public List<ScheduleVO> selectAllSchedule (){
-		return scheduleServiceDAO .selectAllSchedule(); 
-	}
+	private ScheduleDAO scheduleDAO ;
+	
     // 부서에 맞는 내용만 조회
-    public List<ScheduleVO> selectSchedulesByDepartment(ScheduleVO scheduleVO) {
-        return scheduleServiceDAO.selectSchedulesByDepartment(scheduleVO);
+    public List<ScheduleVO> selectSchedulesByDepartment(int department_id) {
+        return scheduleDAO.selectSchedulesByDepartment(department_id);
     }
 
     // 일정 추가
     public int insertSchedule(ScheduleVO scheduleVO) {
-        return scheduleServiceDAO.insertSchedule(scheduleVO);
+        return scheduleDAO.insertSchedule(scheduleVO);
     }
 
     // 일정 수정
     public int updateSchedule(ScheduleVO scheduleVO) {
-        return scheduleServiceDAO.updateSchedule(scheduleVO);
+        return scheduleDAO.updateSchedule(scheduleVO);
     }
 
     // 일정 삭제
     public int deleteSchedule(ScheduleVO scheduleVO) {
-    	return scheduleServiceDAO.deleteSchedule(scheduleVO);
+    	return scheduleDAO.deleteSchedule(scheduleVO);
     }
 
 }
